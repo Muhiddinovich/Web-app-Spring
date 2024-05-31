c3a6458346ccb9b5e2cbab306447f18744ea48f9
Fakhriddin Mullagoziev <mullagoziev@gmail.com>
2024-05-31 23:12:57 +0500

    Redirect to a JSP using Spring Boot

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -7,11 +7,36 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class SayHelloController {
 	// "say-hello" => "Hello! What are you learning today?"
-	//http://localhost:8080/say-hello
+	// http://localhost:8080/say-hello
 	@RequestMapping("say-hello")
 	@ResponseBody
 	public String sayHello() {
 		return "Hello! What are you learning today?";
+	}
+
+	@RequestMapping("say-hello-html")
+	@ResponseBody
+	public String sayHelloHtml() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("<html>");
+		sb.append("<head>");
+		sb.append("<title>My First HTML Page - Changed</title>");
+		sb.append("</head>");
+		sb.append("<body>");
+		sb.append("My first html page with body - Changed");
+		sb.append("</body>");
+		sb.append("</html>");
+
+		return sb.toString();
+	}
+
+	// "say-hello-jsp" => sayHello.jsp
+	// /src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
+	// Spring MVC already knows up to /src/main/resources/META-INF/resources
+	@RequestMapping("say-hello-jsp")
+//	@ResponseBody  removing the annotation
+	public String sayHelloJsp() {
+		return "sayHello";
 	}
 
 }
