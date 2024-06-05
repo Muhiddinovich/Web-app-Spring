defd268f8c4e2cc54a886f51c06060b67c703e7c
Fahriddin <Fahriddin@WIN-U2794NDE3MO>
2024-06-05 04:42:17 -0400

    Creating Login Form

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,20 +1,15 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-	// http://localhost:8080/login?name=Ranga
+	// http://localhost:8080/login
 
 	@RequestMapping("login")
-	public String login(@RequestParam String name, ModelMap model) {
-		model.put("name", name);
+	public String login() {
 		return "login";
 	}
 }
--- myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -1,7 +1,13 @@
 <html>
 <head>
-<title>Login Page</title>
+    <title>Login Page</title>
 </head>
-<body>Welcome to the login page ${name}!
+<body>
+    Welcome to the login page!
+    <form method="post">
+        Name: <input type="text" name="name">
+        Password: <input type = "password" name ="password">
+        <input type="submit">
+    </form>
 </body>
 </html>
